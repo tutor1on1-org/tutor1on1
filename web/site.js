42478a32b8fefeb0a5807b326480644334c69592
kl <kl@KLC832>
2026-04-13 18:19:16 +0800

    Publish client v1.0.28

--- web/site.js
+++ web/site.js
@@ -52,8 +52,8 @@
   ]
   const releaseConfig = Object.freeze({
     githubRepo: 'tutor1on1-org/tutor1on1',
-    appVersion: '1.0.27',
-    releaseTag: 'v1.0.27',
+    appVersion: '1.0.28',
+    releaseTag: 'v1.0.28',
     downloadBaseUrl: 'https://api.tutor1on1.org/downloads',
     assets: Object.freeze({
       android: 'Tutor1on1.apk',
