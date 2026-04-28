90cc3872e4f1319281609e16ce0e9d50f9fb215a
kl <kl@KLC832>
2026-04-28 18:05:00 +0800

    Release admin delete user support

--- web/site.js
+++ web/site.js
@@ -52,8 +52,8 @@
   ]
   const releaseConfig = Object.freeze({
     githubRepo: 'tutor1on1-org/tutor1on1',
-    appVersion: '1.0.41',
-    releaseTag: 'v1.0.41',
+    appVersion: '1.0.42',
+    releaseTag: 'v1.0.42',
     downloadBaseUrl: 'https://api.tutor1on1.org/downloads',
     assets: Object.freeze({
       android: 'Tutor1on1.apk',
